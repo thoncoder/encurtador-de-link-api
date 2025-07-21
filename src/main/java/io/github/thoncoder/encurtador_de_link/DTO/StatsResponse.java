b1e616bf24d323afc6b277fd6526303c63ef58cc
Thalyson Moura <thon.developer@gmail.com>
2025-07-21 10:31:21 -0300

    Resolvendo os conflitos do merge entre main e master

--- src/main/java/io/github/thoncoder/encurtador_de_link/DTO/StatsResponse.java
+++ src/main/java/io/github/thoncoder/encurtador_de_link/DTO/StatsResponse.java
@@ -1,15 +1,11 @@
 package io.github.thoncoder.encurtador_de_link.DTO;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
 
-=======
->>>>>>> 2d9deec4220b2cf97ae6006785a01901646bf061
 public class StatsResponse {
     private String code;
     private String originalUrl;
     private Integer visitas;
-<<<<<<< HEAD
     private LocalDateTime createdAt;
 
     public StatsResponse (String code, String originalUrl, Integer visitas, LocalDateTime createdAt) {
@@ -17,13 +13,6 @@ public class StatsResponse {
         this.originalUrl = originalUrl;
         this.visitas = visitas;
         this.createdAt = createdAt;
-=======
-
-    public StatsResponse (String code, String originalUrl, Integer visitas) {
-        this.code = code;
-        this.originalUrl = originalUrl;
-        this.visitas = visitas;
->>>>>>> 2d9deec4220b2cf97ae6006785a01901646bf061
     }
 
     public String getCode() {
@@ -49,7 +38,6 @@ public class StatsResponse {
     public void setCode(String code) {
         this.code = code;
     }
-<<<<<<< HEAD
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
@@ -58,6 +46,4 @@ public class StatsResponse {
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
-=======
->>>>>>> 2d9deec4220b2cf97ae6006785a01901646bf061
 }
